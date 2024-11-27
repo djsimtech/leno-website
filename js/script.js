bebe965911c0457f5c8062f993de8b093b32736d
Danny Simmons <danny.simmons@cavco.com>
2024-11-27 11:20:09 -0700

    Video Modal

--- js/script.js
+++ js/script.js
@@ -1,14 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Mobile Menu
   const toggleButton = document.querySelector(".navbar__mobile-menu-toggle");
   const mobileMenu = document.querySelector(".navbar__mobile-menu-items");
 
   toggleButton.addEventListener("click", function () {
     mobileMenu.classList.toggle("active");
   });
+
+  // Video Modal
+  const modal = document.getElementById("videoModal");
+  const videoButton = document.querySelector(".preview__video-button");
+  const closeButton = document.querySelector(".modal__close-button");
+  const videoPlayer = document.getElementById("videoPlayer");
+
+  // Open Model when clicked
+  videoButton.addEventListener("click", function () {
+    // Show Modal
+    modal.style.display = "block";
+
+    // Replace the src attribute with the video URL
+    videoPlayer.src = "https://www.youtube.com/embed/8sXRyHI3bLw";
+
+    // Close modal on close button click
+    closeButton.addEventListener("click", function () {
+      modal.style.display = "none";
+      videoPlayer.src = "";
+    });
+
+    // Close modal on outer click
+    window.addEventListener("click", function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+        videoPlayer.src = "";
+      }
+    });
+  });
 });
 
+// Navigation Backgroun On Scroll
 window.addEventListener("scroll", function () {
-  const navbar = this.document.querySelector("navbar");
+  const navbar = document.querySelector("navbar");
 
   if (this.window.scrollY > 0) {
     navbar.classList.add("navbar--scroll");
